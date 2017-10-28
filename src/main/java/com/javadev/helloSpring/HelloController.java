5cf78bee9d969b77b9ecb3c8e00a7f6c669ac443
Kamil <kamil.iskra@outlook.com>
2017-10-28 18:30:10 +0200

    Add simple controller

---
+++ src/main/java/com/javadev/helloSpring/HelloController.java
@@ -0,0 +1,13 @@
+package com.javadev.helloSpring;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Spring Boot!";
+    }
+}
